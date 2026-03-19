0cea6afb317774e2d33d9769cac526b32d7d6fca
AX <ax.hospedagem@gmail.com>
2026-03-19 17:54:25 -0300

    Site DTI para Ebosque, centalização de serviços

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import Banner from './components/Banner.vue'
+
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.png" width="525" height="525" />
+<!---
+    <div>
+      <div>
+        <Banner src="assets/logo.png">
+
+        </Banner>
+      </div>  
+
+    </div> 
+-->
+  </header>
+
+  <main>
+    <div class="wrapper">
+      <HelloWorld msg="Portal EBosque" />
+    </div>
+    <TheWelcome />
+  </main>
+</template>
+,
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/Banner.vue
@@ -0,0 +1,54 @@
+<template>
+  <section class="banner">
+    <!-- Imagem recebida pela prop -->
+    <img :src="image" alt="Banner Image" class="banner-image" />
+
+    <!-- Conteúdo sobreposto -->
+    <div class="banner-content">
+      <slot></slot>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: "Banner",
+  props: {
+    image: {
+      type: String,
+      required: true
+    }
+  }
+})
+</script>
+
+<style scoped>
+.banner {
+  width: 100%;
+  height: 100vh; /* ocupa toda a altura da tela */
+  position: relative;
+  top: 0; /* garante que fique no topo */
+  left: 0;
+  overflow: hidden;
+}
+
+.banner-image {
+  width: 100%;
+  height: 100%;
+  object-fit: cover; /* cobre toda a área sem distorcer */
+  display: block;
+}
+
+.banner-content {
+  position: absolute;
+  top: 50%; /* centraliza verticalmente */
+  left: 50%; /* centraliza horizontalmente */
+  transform: translate(-50%, -50%);
+  color: white;
+  font-size: 32px;
+  font-weight: bold;
+  text-align: center;
+}
+</style>
